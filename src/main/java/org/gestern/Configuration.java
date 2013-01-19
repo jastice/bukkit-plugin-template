27959b32b714700357412beb04fe5bdbbdaa33e2
Justin Kaeser <jast@zno.de>
2013-01-19 15:31:22 +0100

    Initial commit

---
+++ src/main/java/org/gestern/Configuration.java
@@ -0,0 +1,34 @@
+package org.gestern;
+
+import java.util.logging.Logger;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+/**
+ * Singleton for global configuration information. 
+ * Values are initialized when the plugin is enabled.
+ * 
+ * @author jast
+ *
+ */
+public enum Configuration {
+	
+	/** Central configuration instance. */
+    CONF;
+    
+    @SuppressWarnings("unused")
+    private final Logger log = MyPlugin.P.getLogger();
+
+    public boolean variable = false;
+    
+    /**
+     * Set configuration form values in a file configuration.
+     * @param savedConf
+     */
+    public void readConfig(FileConfiguration savedConfig) {
+        
+    	this.variable = savedConfig.getBoolean("variable",false);
+    }
+    
+    
+}
---
+++ src/main/java/org/gestern/MyListener.java
@@ -0,0 +1,7 @@
+package org.gestern;
+
+import org.bukkit.event.Listener;
+
+public class MyListener implements Listener {
+
+}
